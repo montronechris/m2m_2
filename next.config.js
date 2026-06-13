52d22002d1ca501db1483bd415fa10caa830b7a9
montronechris <ggxblood07@gmail.com>
2026-06-13 19:30:37 +0200

    fix project

--- next.config.js
+++ next.config.js
@@ -2,10 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
 
-<<<<<<< HEAD
-=======
-  // Consente accesso in rete locale (opzionale)
->>>>>>> e2c572376d9daa8f531ce90b6c321da5351904ab
   allowedDevOrigins: ["192.168.1.121"],
 
   images: {
